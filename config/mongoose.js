83424157657acc071559a27292763f25043f2425
Moses Ngone <moses@ninelabs.com>
2014-10-19 11:43:20 -0400

    Models update

--- config/mongoose.js
+++ config/mongoose.js
@@ -16,8 +16,8 @@ var config = require('./env');
 
 // load models
 // require(config.root + '/api/events/Event.model');
-require(config.root + '/api/members/Member');
-require(config.root + '/api/members/Roster');
+require(config.root + '/lib/models/Member');
+require(config.root + '/lib/models/Roster');
 
 module.exports = function(mongoose) {
     mongoose.connect(config.database.uri);
